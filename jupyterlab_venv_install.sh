0cf98b18558ea8b9b1bec7263f0322ad9a93bee6
Adam Cohn <38065591+adcoh@users.noreply.github.com>
2020-11-01 19:18:59 +0200

    Update and rename to jupyterlab_venv_install.sh

--- jupyterlab_venv_install.sh
+++ jupyterlab_venv_install.sh
@@ -1,23 +1,15 @@
-venv_root_dir=${PWD##*/}
-
 pip install --upgrade pip
-
-pip install jupyterlab
-
+pip install upgrade jupyterlab
 pip install autopep8
-
 pip install --upgrade jupyterlab-git
 
+venv_root_dir=${PWD##*/}
 python -m ipykernel install --user --name=$venv_root_dir
 
 pip install ipywidgets
-
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
-
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
-
 jupyter labextension install jupyterlab-execute-time
-
 jupyter serverextension enable --py jupyterlab_git
 
 jupyter lab build
