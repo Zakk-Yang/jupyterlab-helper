0f76146ae0c24a99cff91fe840942da4197eebde
Zakk-Yang <zakkyang@hotmail.com>
2021-12-05 13:06:28 +0000

    comment out extention

--- jupyterlab_venv_installer.sh
+++ jupyterlab_venv_installer.sh
@@ -9,9 +9,9 @@ venv_root_dir=${PWD##*/}
 python -m ipykernel install --user --name=$venv_root_dir
 
 pip install ipywidgets
-jupyter nbextension enable --py widgetsnbextension --sys-prefix
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install jupyterlab-execute-time
-jupyter serverextension enable --py jupyterlab_git
+#jupyter nbextension enable --py widgetsnbextension --sys-prefix
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager
+#jupyter labextension install jupyterlab-execute-time
+#jupyter serverextension enable --py jupyterlab_git
 
 jupyter lab build
